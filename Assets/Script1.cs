d74efc9d2a9990291cba6c185cb5e0f9c3b74c87
Syapog <anton.draganov2@gmail.com>
2024-09-01 14:35:18 +0500

    Created

---
+++ Assets/Script1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Script1 : MonoBehaviour
+{
+    public Script2 scr;
+    public int s = 0;
+    public TextMeshPro t;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball" && scr.n == true)
+        {
+            s++;
+            scr.n = false;
+        }
+    }
+
+    public void Update()
+    {
+        t.text = s.ToString();
+    }
+}
---
+++ Assets/Script2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script2 : MonoBehaviour
+{
+    public bool n = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            n = true;
+        }
+    }
+}
