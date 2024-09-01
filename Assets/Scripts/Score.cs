a67ed2e9d8471e69d877b702ffe046e9bfea7c6f
Syapog <anton.draganov2@gmail.com>
2024-09-01 14:27:33 +0500

    Created project

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public TextMeshPro my_text;
+    private int i = 0;
+
+    private void Start()
+    {
+        i = 0;
+        my_text.text = " " + i.ToString();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            i++;
+            my_text.text = " " + i.ToString();
+           
+        }
+       
+    }
+
+  
+}
